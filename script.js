21721cb654b0c4e8e10ac0e146152b60e56d9ee1
Alexandr Trunin <lubomir1976@rambler.ru>
2022-06-23 16:50:35 +0300

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const nameProduct = ('Колбаса');
+const nubberProduct = 2;
+const categoryProduct = 'Полуфабрикаты';
+const priceProduct = 590;
+let price = nubberProduct * priceProduct;
+console.log(price);
