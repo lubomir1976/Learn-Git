ddc0c424436317cfc7483bb285961db242f0179e
Alexandr Trunin <lubomir1976@rambler.ru>
2022-06-23 16:53:39 +0300

    second commit

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const categoryProduct = 'Полуфабрикаты';
 const priceProduct = 590;
 let price = nubberProduct * priceProduct;
 console.log(price);
+
+{
+   const item = 'Мороженное';
+   const count = 15;
+   const category = 'Молочные продукты';
+   const price = 95;
+
+   console.log(item);
+   console.log(`общая сумма товара: ${count * price}`);
+ }
